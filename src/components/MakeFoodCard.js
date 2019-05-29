8ed1846fcb37486af9080b7483098809a0ab282b
James Dorr <jamesdorr3@gmail.com>
2019-05-29 09:34:53 -0400

    added min 0 and step any to number input

--- src/components/MakeFoodCard.js
+++ src/components/MakeFoodCard.js
@@ -78,36 +78,36 @@ class MakeFoodCard extends React.Component {
           </li>
           <li>
           <label>Serving Amount</label>
-          <input type='number' value={this.state.serving_unit_amount} onChange={this.handleChange} name='serving_unit_amount' placeholder='serving amount' />
+          <input type='number' step="any" min='0' value={this.state.serving_unit_amount} onChange={this.handleChange} name='serving_unit_amount' placeholder='serving amount' />
           </li>
           <li>
           <label>Serving Unit</label>
           <select name='serving_unit_name' value={this.state.serving_unit_name} onChange={this.handleChange} placeholder="unit">
-              {['unit','package','piece'].map(measure => {
+              {['unit','tbsp','package','piece', 'small', 'medium', 'large'].map(measure => {
                 return <option value={measure} name='measure' >{measure} </option>}
               )}
           </select>
           </li>
           <li>
             <label>Serving in Grams</label>
-            <input type='number' value={this.state.serving_grams} onChange={this.handleChange} name='serving_grams' placeholder='grams/serving' />
+            <input type='number' step="any" min='0' value={this.state.serving_grams} onChange={this.handleChange} name='serving_grams' placeholder='grams/serving' />
           </li>
           {/* <li></li> */}
           <li>
             <label>Calories</label>
-            <input type='number' value={this.state.calories} onChange={this.handleChange} name='calories' placeholder='Calories' />
+            <input type='number' step="any" min='0' value={this.state.calories} onChange={this.handleChange} name='calories' placeholder='Calories' />
           </li>
           <li>
             <label>Fat</label>
-            <input type='number' value={this.state.fat} onChange={this.handleChange} name='fat' placeholder='fat' />
+            <input type='number' step="any" min='0' value={this.state.fat} onChange={this.handleChange} name='fat' placeholder='fat' />
           </li>
           <li>
             <label>Carbs</label>
-            <input type='number' value={this.state.carbs} onChange={this.handleChange} name='carbs' placeholder='carbs' />
+            <input type='number' step="any" min='0' value={this.state.carbs} onChange={this.handleChange} name='carbs' placeholder='carbs' />
           </li>
           <li>
             <label>Protein</label>
-            <input type='number' value={this.state.protein} onChange={this.handleChange} name='protein' placeholder='protein' />
+            <input type='number' step="any" min='0' value={this.state.protein} onChange={this.handleChange} name='protein' placeholder='protein' />
           </li>
           <li>
             <input type='submit' />
@@ -119,35 +119,35 @@ class MakeFoodCard extends React.Component {
         <ul className='newFoodGrid' style={{display: this.state.moreInfoForm ? 'grid' : 'none'}}>
           <li>
             <label>Saturated Fat</label>
-            <input type='number' value={this.state.saturated_fat} onChange={this.handleChange} name='saturated_fat' placeholder='saturated fat' />
+            <input type='number' step="any" min='0' value={this.state.saturated_fat} onChange={this.handleChange} name='saturated_fat' placeholder='saturated fat' />
           </li>
           <li>
             <label>Sugars</label>
-            <input type='number' value={this.state.sugars} onChange={this.handleChange} name='sugars' placeholder='sugars' />
+            <input type='number' step="any" min='0' value={this.state.sugars} onChange={this.handleChange} name='sugars' placeholder='sugars' />
           </li>
           <li>
             <label>Dietary Fiber</label>
-            <input type='number' value={this.state.dietary_fiber} onChange={this.handleChange} name='dietary_fiber' placeholder='dietary fiber' />
+            <input type='number' step="any" min='0' value={this.state.dietary_fiber} onChange={this.handleChange} name='dietary_fiber' placeholder='dietary fiber' />
           </li>
           <li>
             <label>Sodium</label>
-            <input type='number' value={this.state.sodium} onChange={this.handleChange} name='sodium' placeholder='sodium' />
+            <input type='number' step="any" min='0' value={this.state.sodium} onChange={this.handleChange} name='sodium' placeholder='sodium' />
           </li>
           <li>
             <label>Cholesterol</label>
-            <input type='number' value={this.state.cholesterol} onChange={this.handleChange} name='cholesterol' placeholder='cholesterol' />
+            <input type='number' step="any" min='0' value={this.state.cholesterol} onChange={this.handleChange} name='cholesterol' placeholder='cholesterol' />
           </li>
           <li>
             <label>Potassium</label>
-            <input type='number' value={this.state.potassium} onChange={this.handleChange} name='potassium' placeholder='potassium' />
+            <input type='number' step="any" min='0' value={this.state.potassium} onChange={this.handleChange} name='potassium' placeholder='potassium' />
           </li>
           <li>
             <label>Unit Size</label>
-            <input type='number' value={this.state.unit_size} onChange={this.handleChange} name='unit_size' placeholder='unit size' />
+            <input type='number' step="any" min='0' value={this.state.unit_size} onChange={this.handleChange} name='unit_size' placeholder='unit size' />
           </li>
           <li>
             <label>Bar Code / UPC</label>
-            <input type='number' value={this.state.upc} onChange={this.handleChange} name='upc' placeholder='UPC code' />
+            <input type='number' step="any" min='0' value={this.state.upc} onChange={this.handleChange} name='upc' placeholder='UPC code' />
           </li>
           <li></li>
           <li>
