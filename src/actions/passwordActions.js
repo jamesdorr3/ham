c0ef1af34d28e150be0328905050c2689fa3ab93
James Dorr <jamesdorr3@gmail.com>
2019-08-30 18:56:22 -0700

    ugly FORGOT PASSWORD button, working to fix

---
+++ src/actions/passwordActions.js
@@ -0,0 +1,34 @@
+import {URL, HEADERS} from '../constants'
+
+export const forgotPassword = (email) => {
+  return (dispatch) => {
+    return fetch(`${URL}/password/forgot`, {
+      method: 'POST',
+      headers: HEADERS(),
+      body: JSON.stringify({
+        email: email
+      })
+    })
+  }
+}
+
+// export const reauth = () => {
+//   return (dispatch) => {
+//     dispatch({type: 'START_LOADING'})
+//     return fetch(`${URL}/reauth`, {
+//       method: 'POST',
+//       headers: HEADERS()
+//     })
+//     .then(response => {
+//       return response.json()
+//     })
+//     .then(jwtAndUser => {
+//       // console.log(jwtAndUser)
+//       dispatch({type: 'STOP_LOADING'})
+//       if (jwtAndUser.user && jwtAndUser.jwt) {
+//         // console.log(jwtAndUser)
+//         dispatch({ type: 'SELECT_USER', payload: jwtAndUser})
+//       }
+//     })
+//   }
+// }
